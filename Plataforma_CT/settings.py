7b7575ff32dce18e37bfb1702e0c85c7427ab930
Victor2020-Hurtado <vlopez@finkok.com.mx>
2020-10-26 15:24:28 -0600

    combio en empresa

--- Plataforma_CT/settings.py
+++ Plataforma_CT/settings.py
@@ -74,11 +74,14 @@ WSGI_APPLICATION = 'Plataforma_CT.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'base',
+        'USER': 'postgres',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'DATABASE_PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
